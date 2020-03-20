036810dbcfe2c1600ea6d246d9233d8cbe0ed461
Nikunj Thakor <38680557+nnikunjt@users.noreply.github.com>
2020-03-20 11:02:23 +0530

    Confirmation letter pdf is now perfectly formeted

--- resources/views/makepdf.blade.php
+++ resources/views/makepdf.blade.php
@@ -6,18 +6,10 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"><script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	 <script src="{{ asset('js/myscript.js') }}" defer></script>
-	 <script src="{{ asset('js/all.js') }}" defer></script>
-	 <script src="{{ asset('js/jspdf.js') }}" defer></script>
+	 
 
-<script type="text/javascript">
-	window.onload = function() 
-{
-  HTMLtoPDF();
-};
-</script >
 </head>
-<body id="HTMLtoPDF">
+<body>
 
 
 <div>
@@ -25,19 +17,21 @@
 			 <h1 align="center">Registration Confirmation Letter</h1>
 			<h3 align="center">YashClasses, Bharuch</h3>
 	</div>
-	<div style="margin-left: 10rem;">
-		<p>	Date : {{ date('d/m/y')}}</p>
-		<p>Sr No.   : {{ date('yy').$student['sid']}}</p>
-		<p>Name     : {{ $student['fname']}} {{ $student['lname']}}</p>
-		<p>Course   : {{ $student['course']}} {{ $student['sem']}} Sem</p>
-		<p>E-mail   : {{ $student['email'] }}</p>
-		<p>Phone    : {{ $student['phone'] }}</p>
-		<p>Language : {{ $student['language'] }}</p>
+	<div style="margin-top:3rem;">
+		<h6>Date : {{ date('d/m/y')}}</h6>
+		<h6>Sr No.   : {{ date('yy').$student['sid']}}</h6>
+	</div>
+	<div style="margin-left: 13rem;margin-top:2rem;">
+		
+		<p><b>Name    &nbsp; :</b> &nbsp;&nbsp;{{ $student['fname']}} {{ $student['lname']}}</p>
+		<p><b>Course   :</b> &nbsp; {{ $student['course']}} {{ $student['sem']}} Sem</p>
+		<p><b>E-mail   &nbsp;:</b> &nbsp;&nbsp;{{ $student['email'] }}</p>
+		<p><b>Mobile   :</b> &nbsp;&nbsp;{{ $student['phone'] }}</p>
+		<p><b>Language :</b> {{ $student['language'] }}</p>
 	<div>
 </div>
 
 
 </body>
 
-</script>
 </html>
